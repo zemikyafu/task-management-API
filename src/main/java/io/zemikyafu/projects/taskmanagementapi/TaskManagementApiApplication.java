fd73aa423ae4c3d5fa23fc5f70e64e0c6342e571
zemikyafu <zemikfcb@gmail.com>
2024-10-10 18:40:39 +0300

    first commit

---
+++ src/main/java/io/zemikyafu/projects/taskmanagementapi/TaskManagementApiApplication.java
@@ -0,0 +1,13 @@
+package io.zemikyafu.projects.taskmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/zemikyafu/projects/taskmanagementapi/TaskManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zemikyafu.projects.taskmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
